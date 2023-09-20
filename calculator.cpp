e41684aa27adce34ac15356815952dd14819c74e
João Beyer <100128679+DantasBeyer@users.noreply.github.com>
2023-09-20 12:31:28 +0200

    easy calculator

---
+++ calculator.cpp
@@ -0,0 +1,23 @@
+#include <iostream> //incliudes the iostream file
+
+using namespace std; // includes the std = standard library
+
+int main()
+{
+    int a;
+    int b;
+    int sum;
+
+    cout << "Enter the first Number \n";
+    cin >> a;
+
+    cout << "Enter the second Number \n";
+    cin >> b;
+
+    sum= a + b;
+
+    cout << "The sum of the numbers is " << sum << endl;
+
+    return 0;
+}
+
