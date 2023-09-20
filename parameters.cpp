c0b1c03a2014918951f25f3c1fcfd6b54a22367a
João Beyer <100128679+DantasBeyer@users.noreply.github.com>
2023-09-20 14:57:27 +0200

    inicio de parametrers

---
+++ parameters.cpp
@@ -0,0 +1,22 @@
+#include <iostream> //incliudes the iostream file
+
+using namespace std; // includes the std = standard library
+
+
+// Parameter datatype  = int
+// Parameter name = x
+void favoriteNumber( int x){
+
+    cout << "This is my favorite number:" << x << endl;
+
+}
+
+int main (){
+
+    favoriteNumber(7);
+
+    return 0;
+}
+
+
+
